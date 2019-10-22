4c8fcdc2699dec48084a5102a92cbdb222459234
trigunawan89 <tri.gunawan@live.com>
2019-10-22 19:16:49 +0700

    exercise 30 - 34

---
+++ exercise_23.py
@@ -0,0 +1,25 @@
+import sys
+
+script , encoding, error = sys.argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line,encoding, errors)
+        return main(language_file, encoding,errors)
+
+def print_line(line,encoding, errors):
+    next_lang = line.strip()
+    print(1,line)
+    print(2,line.strip())
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+
+    cooked_string = raw_bytes.decode(encoding, errors = errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+language = open("languages.txt", encoding = "utf-8")
+
+main(language, encoding, error)
+
